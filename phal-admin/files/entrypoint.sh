1105ea3b50c39d6cf104b83fea2004f05437d12c
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-12-18 16:26:55 -0500

    [pahl-admin] Keep Python library state

--- phal-admin/files/entrypoint.sh
+++ phal-admin/files/entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # Install PHAL admin plugins via pip command when a setup.py exists
-phal_admin_list=~/.config/mycroft/phal_admin.list
+phal_admin_list=~/.config/mycroft/phal.list
+phal_admin_list_state=/tmp/phal.state
 if test -f "$phal_admin_list"; then
-    pip3 install -r "$phal_admin_list"
+    if ! diff -q -B <(grep -vE '^\s*(#|$)' "$phal_admin_list") <(grep -vE '^\s*(#|$)' "$phal_admin_list_state" 2>/dev/null) &>/dev/null; then
+        pip3 install -r "$phal_admin_list"
+        cp "$phal_admin_list" "$phal_admin_list_state"
+    fi
 fi
 
 # Clear Python cache
