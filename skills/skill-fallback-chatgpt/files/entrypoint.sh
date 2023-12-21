cfccc43fdbcf65adc6377af61fd0c1ebe47c5aa8
Gaëtan Trellu <gaetan.trellu@gmail.com>
2023-12-21 17:31:40 -0500

    [skills/fallback-chatgpt] Add fallback ChatGPT skill

---
+++ skills/skill-fallback-chatgpt/files/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Path to the skill's configuration directory
+config_skill_directory=~/.config/mycroft/skills/skill-ovos-fallback-chatgpt.openvoiceos
+
+# Create the skill's configuration directory if it does not exists
+# and a sample of settings.json file
+if ! test -d "$config_skill_directory"; then
+    mkdir -p "$config_skill_directory"
+    cat <<EOF >"$config_skill_directory/settings.json"
+{
+    "api_key": "sk-XXXXXXXX",
+    "model": "gpt-3.5-turbo",
+    "system_prompt": "You are a helpful voice assistant with a friendly tone and fun sense of humor",
+    "wait_timeout": 90,
+    "__mycroft_skill_firstrun": false
+}
+EOF
+fi
+
+# Run skill-ovos-fallback-chatgpt skill
+ovos-skill-launcher skill-ovos-fallback-chatgpt.openvoiceos
